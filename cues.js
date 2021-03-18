110b90d9947bea3617d7b1b6624789c7c5fdc60a
Julian Hartline <julianh2o@gmail.com>
2021-03-17 22:27:34 -0700

    Removing old code

--- cues.js
+++ cues.js
@@ -3,7 +3,18 @@ const xml2js = require("xml2js");
 const _ = require("lodash");
 const path = require("path");
 const os = require("os");
+const child_process = require('child_process');
+const util = require("util");
 const isWin = process.platform === "win32";
+
+const firstFrom = (a) => a ? a[0] : null;
+const songsMatch = (a,b) => a.$.FileSize === b.$.FileSize && path.basename(a.$.FilePath) === path.basename(b.$.FilePath);
+const calculateMatchHash = (name,size) => `${size}_${name}`;
+const isWindowsPath = (p) => p.indexOf("\\") !== -1;
+const parsePath = (p) => isWindowsPath(p) ? path.win32.parse(p) : path.posix.parse(p);
+const nameForPath = (p) => parsePath(p).base;
+const exec = async (cmd) => util.promisify(child_process.exec)(cmd);
+const execDir = async (dir,cmd) => util.promisify(child_process.exec)(cmd,{cwd:dir});
 
 const findVdjDatabase = (dir) => {
   if (isWin) {
@@ -21,22 +32,6 @@ async function* walk(dir) {
         if (d.isDirectory()) yield* walk(entry);
         else if (d.isFile()) yield entry;
     }
-}
-
-async function doExport(xml,dir) {
-  const parsed = await xml2js.parseStringPromise(xml);
-  const songs = parsed.VirtualDJ_Database.Song;
-  const directorySongs = _.filter(songs,(s) => s.$.FilePath.startsWith(dir));
-  const res = _.map(directorySongs,(s) => {
-    const cues = _.filter(s.Poi,(p) => p.$.Type === "cue");
-    return {
-      "name":path.basename(s.$.FilePath),
-      "path":s.$.FilePath,
-      "size":s.$.FileSize,
-      "cues":_.map(cues,"$")
-    }
-  });
-  console.log(JSON.stringify(res, null, 2),"utf-8");
 }
 
 async function loadDatabase(f) {
@@ -58,39 +53,6 @@ async function saveDatabase(f,db) {
   fs.writeFileSync(f,xml,"utf-8");
 }
 
-async function doImport(xml,data,dir) {
-  const parsed = await xml2js.parseStringPromise(xml);
-  const songs = parsed.VirtualDJ_Database.Song;
-
-
-  for (const song of data) {
-
-    if (song.cues.length === 0) continue;
-    if (nameAndSizeMatches.length || pathMatches.length) {
-
-
-    }
-    const newSong = {
-      '$':{ FilePath:songPathByName },
-      'Poi':_.map(song.cues,(c) => ({'$':c}))
-    };
-    parsed.VirtualDJ_Database.Song.push(newSong);
-  }
-}
-
-async function loadMemo(jsonFile) {
-  console.log("Loading memo: ",jsonFile);
-  if (!fs.existsSync(jsonFile)) return [];
-  const jsonRaw = fs.readFileSync(jsonFile, "utf-8");
-  return JSON.parse(jsonRaw);
-}
-
-async function saveMemo(jsonFile,memo) {
-  fs.writeFileSync(jsonFile,JSON.stringify(memo,null,2), "utf-8");
-}
-
-const firstFrom = (a) => a ? a[0] : null;
-
 function mergeSongData(from_vdj,from_json) {
   if (!from_vdj) return from_json;
   if (!from_json) return from_vdj;
@@ -100,12 +62,6 @@ function mergeSongData(from_vdj,from_json) {
   res.Infos = [_.merge(firstFrom(from_vdj.Infos),firstFrom(from_json.Infos))];
   res.Scan = [_.merge(firstFrom(from_vdj.Scan),firstFrom(from_json.Scan))];
   res.Poi = [...from_vdj.Poi || [],...from_json.Poi ||[]];
-  //const filteredPoi = _.filter(from_json.Poi,(p) => p.$.Type === "cue" || p.$.Type === "loop");
-  // console.log("from_vdj",_.filter(from_vdj.Poi,(p) => p.$.Type === "cue" || p.$.Type === "loop"))
-  // console.log("from_json",_.filter(from_json.Poi,(p) => p.$.Type === "cue" || p.$.Type === "loop"))
-  //const filteredPoiNames = _.map(filteredPoi,(p) => p.$.Name);
-  //res.Poi = _.reject(res.Poi,p => p.$.Name && _.includes(filteredPoiNames,p.$.Name));
-  //res.Poi = [...res.Poi,...filteredPoi];
   res.Poi = _.uniqBy(res.Poi,(p) => p.$.Name || p.$.Pos);
 
   const cues = new Set();
@@ -113,15 +69,8 @@ function mergeSongData(from_vdj,from_json) {
     if (cues.has(p.$.Num)) delete p.$.Num;
     if (p.$.Num) cues.add(p.$.Num);
   }
-  // console.log("res",_.filter(res.Poi,(p) => p.$.Type === "cue" || p.$.Type === "loop"))
   return res;
 }
-
-const songsMatch = (a,b) => a.$.FileSize === b.$.FileSize && path.basename(a.$.FilePath) === path.basename(b.$.FilePath);
-const calculateMatchHash = (name,size) => `${size}_${name}`;
-const isWindowsPath = (p) => p.indexOf("\\") !== -1;
-const parsePath = (p) => isWindowsPath(p) ? path.win32.parse(p) : path.posix.parse(p);
-const nameForPath = (p) => parsePath(p).base;
 
 async function walkFilesystemForSongs(dir) {
   const songs = [];
@@ -137,73 +86,6 @@ async function walkFilesystemForSongs(dir) {
     }
   }
   return songs;
-}
-
-async function main() {
-  const cwd = __dirname;
-  const mode = process.argv[2] || "sync";
-  const vdjPath = findVdjDatabase(cwd);
-  const db = await loadDatabase(vdjPath);
-  const memoPath = "memo.json";
-  let memo = await loadMemo(memoPath);
-
-  if (mode === "cleandb") {
-    for (const song of memo) {
-      const songPathByName = path.join(cwd,song.name);
-      const clean = _.filter(db.VirtualDJ_Database.Song,(s) => {
-        return path.basename(s.$.FilePath) === song.name && s.$.FileSize === song.size
-          || s.$.FilePath === songPathByName;
-      });
-
-      console.log(`Cleaning: ${clean.length} entry(ies) for ${song.name}`);
-      db.VirtualDJ_Database.Song = _.difference(db.VirtualDJ_Database.Song,clean);
-    }
-  } else if (mode === "import") {
-    const xml = await doImport(vdj,json,cwd);
-    fs.writeFileSync(vdjPath,xml,"utf-8");
-  } else if (mode === "lib") {
-    const importDatabasePath = process.argv[3];
-    const localRoot = process.argv[4];
-
-    const importDatabase = await loadDatabase(importDatabasePath);
-    const filesystemSongs = walkFilesystemForSongs(localRoot);
-
-    const dbByNameSize = {}
-    for (const s of importDatabase.VirtualDJ_Database.Song) {
-
-      const size = s.$.FileSize;
-      const key = `${size}_${name}`;
-      allKeys.add(key);
-      dbByNameSize[key] = s;
-    };
-  } else if (mode === "sync") {
-    const directorySongs = _.filter(db.VirtualDJ_Database.Song,(s) => s.$.FilePath.startsWith(cwd));
-    if (_.uniqWith(directorySongs,(a,b) => songsMatch(a,b)).length != directorySongs.length) {
-      console.log("duplicate songs, abort!");
-      process.exit()
-    }
-    for (const from_vdj of directorySongs) {
-      const from_json = _.find(memo,(s) => songsMatch(s,from_vdj));
-
-      const updated = mergeSongData(from_vdj,from_json);
-      memo = _.reject(memo,(s) => songsMatch(s,from_vdj));
-      memo.push(updated);
-    }
-
-    for (const from_json of memo) {
-      const from_vdj = _.find(db.VirtualDJ_Database.Song,(s) => songsMatch(s,from_json));
-      if (from_vdj) {
-        const updated = mergeSongData(from_vdj,from_json);
-        db.VirtualDJ_Database.Song = _.reject(db.VirtualDJ_Database.Song,(s) => songsMatch(s,from_json));
-        db.VirtualDJ_Database.Song.push(updated);
-      } else {
-        db.VirtualDJ_Database.Song.push(from_json);
-      }
-    }
-  }
-
-  saveDatabase(vdjPath,db);
-  saveMemo(memoPath,memo);
 }
 
 function mergeDatabases(a,b) {
@@ -230,7 +112,11 @@ function updateDatabasePaths(db,localFiles) {
 
 async function main2() {
   const localRoot = process.argv[2];
-  const importDatabasePath = process.argv[3];
+  const importRepositoryPath = process.argv[3];
+
+  await execDir(importRepositoryPath,"git pull origin master");
+
+  const importDatabasePath = path.join(importRepositoryPath,"database.xml");
 
   const localDatabasePath = findVdjDatabase(localRoot);
   const localDatabase = await loadDatabase(localDatabasePath);
@@ -239,12 +125,18 @@ async function main2() {
   const localSongs = await walkFilesystemForSongs(localRoot);
 
   const newDatabase = mergeDatabases(localDatabase,importDatabase);
-  updateDatabasePaths(newDatabase,localSongs);
 
+  //Update the repository version
+  console.log("Updating repository database.xml...");
+  saveDatabase(importDatabasePath,newDatabase);
+  await execDir(importRepositoryPath,"git add database.xml");
+  await execDir(importRepositoryPath,`git commit -m 'Updated database.xml from ${os.hostname()}'`);
+  await execDir(importRepositoryPath,`git push origin master`);
+
+  //Update the local database
+  console.log("Updating local database.xml...");
+  updateDatabasePaths(newDatabase,localSongs);
   saveDatabase(localDatabasePath,newDatabase);
 }
 
-function main3() {
-}
-
-main3();
+main2();
